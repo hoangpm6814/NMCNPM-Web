146dbb0b39e84245f72d7ca69f56a8d904b57297
unknown <hoangvinh175@gmail.com>
2019-12-16 15:35:18 +0700

    nhập và thanh toán hóa đơn (sp, số lượng, tổng tiền), xuất hóa đơn (pdf/email)

---
+++ public/js/GioHang.js
@@ -0,0 +1,78 @@
+// function foo() {
+//     sessionStorage.setItem("giaTien", document.getElementById("giaTienOtChuong").textContent);
+//     return true;
+// }
+
+
+(function ($) {
+    $("document").ready(function () {
+        sessionStorage.setItem("daTinhTien", "false");
+    });
+    $("#tinhTienBtn").click(function () {
+        var productName = [];
+        $(".product-name").each(function() {
+            productName.push($(this).children().text());
+        });
+
+        var price = [];
+        $("td.price").each(function() {
+            price.push(Number.parseInt($(this).text()));
+        });
+
+        var quantity = [];
+        var isValidQuantity = true;
+        $("input.quantity").each(function() {
+            if ($(this).val() <= 0) {
+                isValidQuantity = false;
+                return;
+            }
+            quantity.push($(this).val());
+        });
+
+        if (!isValidQuantity) {
+            window.alert("Số lượng nhập không hợp lệ");
+            sessionStorage.setItem("daTinhTien", "false");
+            return;
+        }
+
+        var total = [];
+        $(".total").each(function(index) {
+            var totalOneItem = price[index] * quantity[index];
+            $(this).text(totalOneItem)
+            total.push(totalOneItem);
+        });
+
+        var i;
+        var sumPriceOfAllItem = 0;
+        for (item of total) {
+            sumPriceOfAllItem = sumPriceOfAllItem + item;
+        }
+        $(".cart-total").eq(2).children("p").eq(0).children().eq(1).text(sumPriceOfAllItem);
+        $(".cart-total").eq(2).children("p").eq(3).children().eq(1).text(sumPriceOfAllItem);
+        //$(".total-price").children().eq(1).text(sum);
+
+        var donHangHTML = "";
+        var donHang = "";
+        for (i = 0; i < productName.length; i++) {
+            donHangHTML = donHangHTML + '<div class="order-col"> <div>' + quantity[i] + "x " + productName[i] + "</div>";
+            donHangHTML = donHangHTML + "<div>" + total[i] + "</div>";
+            donHang = donHang + quantity[i] + "x " + productName[i] + "\n" + total[i] + "\n"; 
+        }
+        donHang.trimRight();
+
+        sessionStorage.setItem("donHangHTML", donHangHTML);
+        sessionStorage.setItem("sumPriceOfAllItem", sumPriceOfAllItem);
+        sessionStorage.setItem("donHang", donHang);
+        sessionStorage.setItem("daTinhTien", "true");
+    });
+
+    $("#chuyenSangThanhToanBtn").click(function() {
+        if (sessionStorage.getItem("daTinhTien") == "true") {
+            return true;
+        }
+        else {
+            window.alert("Vui lòng bấm nút tính tiền trước khi thanh toán");
+            return false;
+        }
+    });
+})(jQuery);
---
+++ public/js/ThanhToan.js
@@ -0,0 +1,127 @@
+function foo() {
+    document.getElementById("caChua").innerHTML = sessionStorage.getItem("giaTien");
+}
+
+(function ($) {
+    $("document").ready(function () {
+        var donHangHTML = sessionStorage.getItem("donHangHTML");
+        $(".order-products").append(donHangHTML);
+        var sumPriceOfAllItem = sessionStorage.getItem("sumPriceOfAllItem");
+        $(".order-total").text(sumPriceOfAllItem);
+    });
+    $("#xuatHoaDonBtn").click(function () {
+        var firstName1 = $("[name = 'first-name']").eq(0).val();
+        var firstName2 = $("[name = 'first-name']").eq(1).val();
+        var lastName1 = $("[name = 'last-name']").eq(0).val();
+        var lastName2 = $("[name = 'last-name']").eq(1).val();
+        var email1 = $("[name = 'email']").eq(0).val();
+        var email2 = $("[name = 'email']").eq(1).val();
+        var address1 = $("[name = 'address']").eq(0).val();
+        var address2 = $("[name = 'address']").eq(1).val();
+        var city1 = $("[name = 'city']").eq(0).val();
+        var city2 = $("[name = 'city']").eq(1).val();
+        var tel1 = $("[name = 'tel']").eq(0).val();
+        var tel2 = $("[name = 'tel']").eq(1).val();
+        if (firstName1 == "" || firstName2 == "" || lastName1 == "" || lastName2 == "" || email1 == "" || email2 == ""
+        || address1 == "" || address2 == "" || city1 == "" || city2 == "" || tel1 == "" || tel2 == "") {
+            window.alert("Vui lòng điền đầy đủ thông tin");
+            return;
+        }
+
+        if (!validateEmail(email1) || !validateEmail(email2)) {
+            window.alert("Email không hợp lệ");
+            return;
+        }
+
+        var doc = new jsPDF();
+        var txt = "CỬA HÀNG CUNG CẤP RAU CỦ VÀ TRÁI CÂY TƯƠI \n \n";
+        txt = txt + "THÔNG TIN NGƯỜI BÁN \n";
+        txt = txt + "Người bán: rau@gmail.com \n"
+        txt = txt + "Địa chỉ: 227 Nguyễn Văn Cừ, Phường 4, Quận 5, TPHCM \n";
+        txt = txt + "Điện thoại: 0977777879 \n";
+        txt = txt + "Email: info@yourdomain.com \n \n";
+        txt = txt + "THÔNG TIN NGƯỜI MUA \n";
+        txt = txt + "Họ tên: " + firstName1 + lastName1 + "\n";
+        txt = txt + "Email: " + email1 + " \n";
+        txt = txt + "Địa chỉ: " + address1 + " \n";
+        txt = txt + "Thành phố: " + city1 + " \n";
+        txt = txt + "Điện thoại: " + tel1 + " \n \n";
+        txt = txt + "ĐƠN HÀNG ĐÃ ĐẶT\n";
+        txt = txt + sessionStorage.getItem("donHang") + "Thành tiền: " + sessionStorage.getItem("sumPriceOfAllItem") + "\n \n";
+        txt = txt + "ĐỊA CHỈ VẬN CHUYỂN \n";
+        txt = txt + "Họ tên: " + firstName2 + lastName2 + "\n";
+        txt = txt + "Email: " + email2 + "\n";
+        txt = txt + "Địa chỉ: " + address2 + "\n";
+        txt = txt + "Thành phố: " + city2 + "\n";
+        txt = txt + "Điện thoại: " + tel2 + "\n";
+
+        //doc.addFont('VNI-Times-normal.ttf', 'VNI-Times', 'normal');
+        doc.setFont('Times New Roman');
+        //doc.setFont('VNI-Times', 'normal');
+        doc.text(change_alias(txt), 10, 5);
+        doc.save('a4.pdf');
+
+        // mailDoc({
+        //     bUI: true,
+        //     cTo: "hoangvinh175@gmail.com",
+        //     cSubject: "Beta Test Report",
+        //     cMsg: "This attached PDF form has been completed as requested. Thanks from George Howard for completing this form. He will use this information to improve his website."
+        // });
+        // window.open('mailto:hoangvinh175@gmail.com');
+
+        // Email.send({
+        //     Host : "local",
+        //     Username : "username",
+        //     Password : "password",
+        //     To : 'hoangvinh175@gmail.com',
+        //     From : "you@isp.com",
+        //     Subject : "This is the subject",
+        //     Body : "And this is the body"
+        //     }).then(
+        //       message => alert(message)
+        //     );
+
+        Email.send({
+            //SecureToken: "4fdd6f80-f32a-4628-a12a-42b4721af4e4",
+            //Host: "smtp@gmail.com",
+            //SecureToken: "d8105e23-ca9b-442b-934d-b5bac2ae983a",
+            SecureToken: "10e93cbf-74b9-4c3f-bc8b-0aa3bb398d58",
+            Username: "hoangvinh175@gmail.com",
+            Password: "8C19E4325D9D94B1BD5BB90C0303BEC8E2F6",
+            To: email2,
+            From: "hoangvinh175@gmail.com",
+            Subject: "HÓA ĐƠN CỬA HÀNG RAU",
+            Body: txt.replace(/\n/g, "<br/>")
+        }).then(
+            message => alert("Kết quả gửi đến email " + email2 + ": " + message)
+        );
+
+    });
+
+    function change_alias(alias) {
+        var str = alias;
+        str = str.replace(/à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ/g,"a");
+        str = str.replace(/À|Á|Ạ|Ả|Ã|Â|Ầ|Ấ|Ậ|Ẩ|Ẫ|Ă|Ằ|Ắ|Ặ|Ẳ|Ẵ/g,"A");
+        str = str.replace(/è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ/g,"e");
+        str = str.replace(/È|É|Ẹ|Ẻ|Ẽ|Ê|Ề|Ế|Ệ|Ể|Ễ/g,"E");
+        str = str.replace(/ì|í|ị|ỉ|ĩ/g,"i");
+        str = str.replace(/Ì|Í|Ị|Ỉ|Ĩ/g,"I");
+        str = str.replace(/ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ/g,"o");
+        str = str.replace(/Ò|Ó|Ọ|Ỏ|Õ|Ô|Ồ|Ố|Ộ|Ổ|Ỗ|Ơ|Ờ|Ớ|Ợ|Ở|Ỡ/g,"O"); 
+        str = str.replace(/ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ/g,"u");
+        str = str.replace(/Ù|Ú|Ụ|Ủ|Ũ|Ư|Ừ|Ứ|Ự|Ử|Ữ/g,"U"); 
+        str = str.replace(/ỳ|ý|ỵ|ỷ|ỹ/g,"y");
+        str = str.replace(/Ỳ|Ý|Ỵ|Ỷ|Ỹ/g,"Y"); 
+        str = str.replace(/đ/g,"d");
+        str = str.replace(/Đ/g,"D");
+        //str = str.replace(/!|@|%|\^|\*|\(|\)|\+|\=|\<|\>|\?|\/|,|\.|\:|\;|\'|\"|\&|\#|\[|\]|~|\$|_|`|-|{|}|\||\\/g," ");
+        //str = str.replace(/ + /g," ");
+        //str = str.trim(); 
+        return str;
+    }
+
+    function validateEmail(email) {
+        var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(String(email).toLowerCase());
+    }
+})(jQuery);
